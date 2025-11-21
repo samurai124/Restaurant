0015db961b7db53a14d98657df7391b696362088
ennafiaasma-art <ennafiaasma@gmail.com>
2025-11-21 10:21:34 +0100

    cliet serveur

--- IdeaProjects/meniprojet/src/Main.java
+++ IdeaProjects/meniprojet/src/Main.java
@@ -3,10 +3,7 @@ import Serveur.Serveur;
 
 public class Main {
     public  void main (String[]args){
-        Serveur serveur=new Serveur(1,"ser1");
-        Client client =new Client();
-        client.setVip(false);
-        client.PasserCommend();
+
 
 
     }
